088e891cb9a67b6166791ef3ecdbc31b26d849a0
Giovanni Mambretti <71758759+giomambre@users.noreply.github.com>
2024-12-03 00:08:47 +0100

    Update main.py
    Added activity box and little layout edit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_time():
 
 
 def start_session():
-    start_session_button.config(bg='black')
+    open_week_plan_button.config(bg='black')
     
 
 
@@ -32,14 +32,15 @@ root.minsize(600,500)
 root.columnconfigure((0,1,2),weight=1, uniform="a")
 root.rowconfigure(0,weight=1, uniform="a")
 root.rowconfigure(1,weight=3, uniform="a")
-root.rowconfigure(2,weight=4, uniform="a")
+root.rowconfigure(2,weight=3, uniform="a")
+root.rowconfigure(3,weight=2, uniform="a")
 
 #Nav / Menu bar open button
 
 nav_bar_img = Image.open("./img/9293128.png").resize((35,35))
 nav_bar_img_tk = ImageTk.PhotoImage(nav_bar_img)
 nav_bar_button = Button(root, image = nav_bar_img_tk, width="50", height="50",bg="white" )
-nav_bar_button.grid(column=0,row=0,sticky="nws")
+nav_bar_button.grid(column=0,row=0,sticky="nws",padx = 5,pady=5)
 
 #Clock
 
@@ -49,12 +50,32 @@ nav_bar_button.lift()
 get_time()
 
 
-#Start Session Button
+#Set Week Plan Button
 
-start_session_img = Image.open("./img/TEMP_LOGO.png").resize((300,300))
-start_session_img_tk = ImageTk.PhotoImage(start_session_img)
-start_session_button = Button(root,command=start_session, image = start_session_img_tk,  bg="#0F0F0F", width="50", height="50")
-start_session_button.grid(column=1,row=1,sticky="nwse")
+open_week_plan_img = Image.open("./img/TEMP_LOGO.png").resize((200,200))
+open_week_plan_img_tk = ImageTk.PhotoImage(open_week_plan_img)
+open_week_plan_button = Button(root,command=start_session, image = open_week_plan_img_tk,  bg="#0F0F0F", width="50", height="50")
+open_week_plan_button.grid(column=1,row=1,sticky="nwse",padx = 20 ,pady=20)
+
+
+#Label list Activity
+
+activity_label = Label(root, text="Next Activities:", font = ("Verdana", 15,"bold"))
+activity_label.grid(row=1, column=0,sticky="sw",padx=5,pady=10)
+
+#Next Activity to Do
+
+activity = [i for i in range(26)]
+list_activity = StringVar(value=activity)
+activity_listbox = Listbox(root, listvariable=list_activity, height=6,selectmode="extended", font=("Verdana", 10),justify=CENTER )
+activity_listbox.grid(row = 2 , column= 0,sticky="nsew",padx = 5,pady=5)
+
+scrollbar_activity = ttk.Scrollbar(root, orient="vertical", command=activity_listbox.yview) 
+scrollbar_activity.grid(row = 2 , column= 0,sticky="nse")
+
+activity_listbox["yscrollcommand"] = scrollbar_activity.set
+
+
 
 
 
